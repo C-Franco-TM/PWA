321dcaf0b6c669e832c9cf188a4bca1557e9219d
C-Franco-TM <francotmoreira@gmail.com>
2026-06-01 19:25:22 -0300

    Agregar API REST para carreras

---
+++ fei2026tp/backend/aulas/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import CarreraMixin
+
+urlpatterns = [
+    path("carreras/", CarreraMixin.as_view(), name="carrera-list"),
+]
--- fei2026tp/backend/aulas/views.py
+++ fei2026tp/backend/aulas/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+
+from .models import Carrera
+from .serializers import CarreraSerializer
+
+
+class CarreraMixin(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Carrera.objects.all()
+    serializer_class = CarreraSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
